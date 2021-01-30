b20b376aa9b4be2dcfaf80b2b5da597742f209e0
mayanknimcet188 <205119053@nitt.edu>
2021-01-30 13:30:22 +0530

    Added Flask-Login User sign in,sign out and register

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 moment = Moment()
 mail = Mail()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,6 +22,7 @@ def create_app(config_name):
     moment.init_app(app)
     mail.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
